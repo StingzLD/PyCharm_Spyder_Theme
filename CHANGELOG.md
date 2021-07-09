52d64783b09899d78735dcd0eb4446340d0407d7
StingzLD <stingzlightingdesign@hotmail.com>
2021-07-09 11:22:10 -0500

    Created test files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 <!-- Keep a Changelog guide -> https://keepachangelog.com -->
 
-# PyCharm_Spyder_Theme Changelog
+# Spyder Theme Changelog
 
 ## [Unreleased]
 ### Added
---
+++ src/main/test/test.py
@@ -0,0 +1,34 @@
+"""
+Created on Thu Jul  8 22:03:09 2021
+
+@author: Corey Hastings
+GitHub:  /StingzLD
+Twitter: @StingzLD
+"""
+import time
+import os
+
+
+class Class1:
+    def __init__(self, name=None):
+        self.name = name
+
+
+def some_function():
+    try:
+        print(time.timezone)
+    finally:
+        pass
+
+
+testClass = Class1()
+print(testClass.name)
+
+# Check out this comment
+
+time.sleep(0)
+
+while 1 < 2:
+    for i in range(2):
+        print('Do this:')
+    break
